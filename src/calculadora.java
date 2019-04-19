86077cf7e994550d297839c1af695ea10dfe3e4a
paula1827 <47665554+paula1827@users.noreply.github.com>
2019-04-18 19:21:13 -0500

    Update calculadora.java

--- src/calculadora.java
+++ src/calculadora.java
@@ -1,9 +1,41 @@
+import java.io.*;
 
 public class calculadora {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void main(String[] args){
+		BufferedWriter bw=new BufferedWriter (new OutputStreamWriter(System.out)) {
+		BufferedReader br=new BufferedReader (new InputStreamReader(System.in)) {
+				
+		public int suma(int num1, int num2){
+			return num1+num2;
+		}
+		public int Resta(int num1, int num2) {
+			return num1-num2;
+		}
+		public int Multiplicacion(int num1, int num2){
+			int resultado = 0;
+			for(int i=1; i<=num2;i++) {
+				resultado=resultado + num1;
+			}
+		return resultado;
+		}
+		public double division(int num1, int num2) {
+				if(num2 !=0) {
+				return num1/(num2*1.0);
+				}
+				else {
+				return Double.NaN;
+					}
+		}
+		public double porcentaje(int num,double porc) {
+			if (porc >=0.0 && porc <=1.0) {
+			return num*porc;
+			}
+				else {
+				return Double.NaN;
+					}	
+		}
 	}
-
+	}
+	}
 }
