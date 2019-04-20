5b540665a280613df4d3b48b7a3f5498fd2448b3
paula1827 <47665554+paula1827@users.noreply.github.com>
2019-04-20 13:53:55 -0500

    Update calculadora.java

--- src/calculadora.java
+++ src/calculadora.java
@@ -101,3 +101,79 @@ public class calculadora {
 		
 	}
 }	
+
+__________________________________________________________________________________________
+import java.io.*;
+import java.text.BreakIterator;
+import java.util.Scanner;
+
+public class calculadora {
+	
+	public static void main (String[] args) {
+		Scanner entrada = new Scanner(System.in);
+			int num1, num2,resultado,opcion;
+						
+		System.out.println("Menú");
+		System.out.println("1. Suma");
+		System.out.println("2. Resta");
+		System.out.println("3. Multiplicación");
+		System.out.println("4. División");
+		System.out.println("5. Porcentaje");
+		System.out.println("6. Calcular el valor maximo entre tres valores");
+		System.out.println("7. Calcular el promedio entre cinco valores");
+		System.out.println("8. Salir\n");
+		System.out.println("Ingrese la opción que desea");
+		opcion = entrada.nextInt();
+		
+		if (opcion==8) {
+			System.out.println("Salir");
+		} else {
+			switch(opcion) {
+			case 1:
+				System.out.println("Ingrese el primer valor: ");
+				num1 = entrada.nextInt();
+				System.out.println("Ingrese el segundo valor: ");
+				num2 = entrada.nextInt();
+				resultado= num1+num2;
+				System.out.println("El resultado de la suma es: " + resultado);
+				break;
+			case 2:
+				System.out.println("Ingrese el primer valor: ");
+				num1 = entrada.nextInt();
+				System.out.println("Ingrese el segundo valor: ");
+				num2 = entrada.nextInt();
+				resultado = num1-num2;
+				System.out.println("El resultado de la resta es: " + resultado);
+				break;
+			case 3:
+				System.out.println("Ingrese el primer valor: ");
+				num1 = entrada.nextInt();
+				System.out.println("Ingrese el segundo valor: ");
+				num2 = entrada.nextInt();
+				resultado = num1*num2;
+				System.out.println("El resultado de la multiplicacion es: " + resultado);
+				break;
+			case 4:
+				System.out.println("Ingrese el primer valor: ");
+				num1 = entrada.nextInt();
+				System.out.println("Ingrese el segundo valor: ");
+				num2 = entrada.nextInt();
+				resultado = num1/num2;
+				System.out.println("El resultado de la division es: " + resultado);
+				break;
+			case 5:
+				System.out.println("Ingrese el primer valor: ");
+				num1 = entrada.nextInt();
+				System.out.println("Ingrese el segundo valor: ");
+				num2 = entrada.nextInt();
+				if (num2 >= 0.0 && num2 <= 1.0) {
+					resultado = num1*num2;
+					System.out.println("El resultado del porcentaje es: " + resultado);
+					break;
+				}
+				
+			}
+		}
+		
+}
+	}
